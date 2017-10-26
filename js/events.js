e29147e3a0bbbd8ca1badae29d63ecc864ef66f7
Katy Carr <khcarr9@gmail.com>
2017-10-26 17:42:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('document').on('keydown', function(key) {
+  $('input').on('keydown', function(key) {
     if(key.which==71) {
       window.alert("You pressed G")
     }
