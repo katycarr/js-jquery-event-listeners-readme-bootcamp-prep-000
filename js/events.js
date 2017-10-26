a121ceb618750c7c7d7cceb0508b978ff5aca408
Katy Carr <khcarr9@gmail.com>
2017-10-26 17:23:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,15 @@
 //define functions here
+function getIt() {
+  window.alert("Hey!");
+}
+
+function frameIt() {
+  
+}
 
 $(document).ready(function(){
-
 // call functions here
+$('p').on('click',getIt);
+
 
 });
