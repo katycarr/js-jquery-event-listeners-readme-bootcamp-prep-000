8f08ab0074db61ff0ca524c57af889c1e96fb16a
Katy Carr <khcarr9@gmail.com>
2017-10-26 17:30:07 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,12 +4,27 @@ function getIt() {
 }
 
 function frameIt() {
-  
+  $('img').class = tasty;
+}
+
+function pressIt(key) {
+  if(key.which===71) {
+    window.alert("You pressed G!")
+  }
+}
+
+function submitIt() {
+  window.alert("Your form is going to be submitted now.")
 }
 
 $(document).ready(function(){
 // call functions here
 $('p').on('click',getIt);
 
+$(document).on('load',frameIt);
+
+$('input').on('keydown',pressIt);
+
+$('form').on('submit',submitIt);
 
 });
